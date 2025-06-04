bed54efafa18bd2bfd18b7b40acb1e94ef6f89f0
dhruvikmodi23 <dhruvikmodi23@gmail.com>
2025-06-04 11:51:08 +0530

    Complete spring Security

--- springsecurity/src/main/java/com/example/springsecurity/config/SecurityConfig.java
+++ springsecurity/src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -1,14 +1,22 @@
 package com.example.springsecurity.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,14 +24,26 @@ import org.springframework.security.web.SecurityFilterChain;
 
 public class SecurityConfig {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Bean
+    public AuthenticationProvider authProvider(){
+
+        DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
+
+        provider.setUserDetailsService( userDetailsService);
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+         return provider;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         //with lambda
-//         http.csrf(customizer -> customizer.disable());
-//         http.authorizeHttpRequests(req -> req.anyRequest().authenticated());
-//        http.formLogin(Customizer.withDefaults());
-//        http.httpBasic(Customizer.withDefaults());
-//        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+         http.csrf(customizer -> customizer.disable());
+         http.authorizeHttpRequests(req -> req.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         // not need to form login when stateless.
 
 
@@ -45,4 +65,24 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+//    @Bean
+//    public UserDetailsService userDetailService(){
+//
+//        UserDetails user = User.
+//                                withDefaultPasswordEncoder().
+//                                username("dhruvik").
+//                                password("abc").
+//                                roles("admin").
+//                                build();
+//
+//        UserDetails user1 = User.
+//                withDefaultPasswordEncoder().
+//                username("dhruvik1").
+//                password("abc").
+//                roles("admin").
+//                build();
+//
+//       return new InMemoryUserDetailsManager(user,user1);
+//    }
 }
---
+++ springsecurity/src/main/java/com/example/springsecurity/dao/UserRepo.java
@@ -0,0 +1,10 @@
+package com.example.springsecurity.dao;
+
+import com.example.springsecurity.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User,Integer> {
+
+    User findByUsername(String username);
+
+}
